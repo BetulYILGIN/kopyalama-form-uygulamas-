7e5bf2c1cd0f05152712036c422ee5812c0db585
Betül YILGIN <101749536+BetulYILGIN@users.noreply.github.com>
2022-03-19 22:15:51 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace kopyalama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.kopyalananlar = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn3.Location = new System.Drawing.Point(14, 165);
+            this.btn3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(636, 33);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "kopyala";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(149, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(501, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(147, 129);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(501, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn1.Location = new System.Drawing.Point(14, 54);
+            this.btn1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(125, 32);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "gözat";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn2.Location = new System.Drawing.Point(12, 125);
+            this.btn2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(125, 32);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "gözat";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 205);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(636, 21);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // kopyalananlar
+            // 
+            this.kopyalananlar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.kopyalananlar.FormattingEnabled = true;
+            this.kopyalananlar.ItemHeight = 18;
+            this.kopyalananlar.Location = new System.Drawing.Point(12, 255);
+            this.kopyalananlar.Name = "kopyalananlar";
+            this.kopyalananlar.Size = new System.Drawing.Size(636, 130);
+            this.kopyalananlar.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "kopyalamak istediğiniz dosyayı seçiniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(289, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "kopyalamak istediğiniz yeri seçiniz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "kopyalananlar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(664, 397);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kopyalananlar);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn3);
+            this.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox kopyalananlar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace kopyalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string kdosya = "";
+        string kdosyaismi="";
+        string ydosya = "";
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+           
+            openFileDialog1.Title = "Kopyalanacak dosyayı seçin.";
+            openFileDialog1.FileName = "";
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                kdosyaismi= openFileDialog1.SafeFileName.ToString();
+                kdosya = openFileDialog1.FileName.ToString();
+                textBox1.Text = kdosya;
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ydosya = folderBrowserDialog1.SelectedPath.ToString();
+                textBox2.Text = ydosya;
+            }
+            
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            File.Copy(kdosya, ydosya + "\\" + kdosyaismi);
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            kopyalananlar.Items.Add(kdosya);
+
+
+            progressBar1.Minimum = 0;
+                progressBar1.Maximum = kdosya.Length;
+                progressBar1.Value = kdosya.Length;
+            
+            
+        }
+    }
+}
